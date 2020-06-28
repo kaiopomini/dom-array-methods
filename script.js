99919d5a30edd3f3974bea98c97c9e95eb382e13
Kaio Pomini <kaiopomini@gmail.com>
2020-06-28 12:36:37 -0300

    addUser function and forEach() to list data

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 let data = [];
 
-getRandomUser()
-getRandomUser()
-getRandomUser()
+
 // Fetch random user and add money
 
 async function getRandomUser() {
@@ -28,4 +26,30 @@ async function getRandomUser() {
 
 function addData(obj){
     data.push(obj)
+
+    updateDOM()
 }
+
+// Update DOM
+
+function updateDOM(provideData = data) {
+    //clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+    provideData.forEach(item => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+        main.appendChild(element)
+    })
+}
+
+// Format number as money
+
+function formatMoney(number) {
+    return '$'+ number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+}
+
+//Event listeners 
+
+addUserBtn.addEventListener('click', getRandomUser)
